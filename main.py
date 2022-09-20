3f688fe7d34f5c28f1de8bdeddb56f6b6914ce25
Tapasya Khatri <tapasya@Tapasyas-MacBook-Air.local>
2022-09-20 13:22:45 -0700

    first commit

--- main.py
+++ main.py
@@ -15,23 +15,37 @@ def init_stage(stage):
 # 4. Any dead cell with 3 neighbors comes alive
 
 def one_generation(stage):
+    active_stage = []
+    for i in range(height):
+        active_stage.append(stage[i].copy())
+
     for v_pos in range(len(stage)):
-        for h_pos in range(len(v_pos)):
+        for h_pos in range(len(stage[v_pos])):
             neighbors = count_neighbors(stage, v_pos, h_pos)
             if not stage[v_pos][h_pos] and neighbors == 3:
-                stage[v_pos][h_pos] = True
+                active_stage[v_pos][h_pos] = True
             elif stage[v_pos][h_pos] and neighbors < 2:
-                stage[v_pos][h_pos] = False
+                active_stage[v_pos][h_pos] = False
             elif stage[v_pos][h_pos] and (neighbors == 2 or neighbors == 3):
-                stage[v_pos][h_pos] = True
+                active_stage[v_pos][h_pos] = True
             elif stage[v_pos][h_pos] and neighbors > 3:
-                stage[v_pos][h_pos] = False
+                active_stage[v_pos][h_pos] = False
+        
+    for v_pos in range(len(stage)):
+        for h_pos in range(len(stage[v_pos])):
+            stage[v_pos][h_pos] = active_stage[v_pos][h_pos]
+
+
                 
 
 init_stage(stage)
+print_stage(stage)
 print("First Generation:")
 print_stage(stage)
 one_generation(stage)
 print("Second Generation:")
 print_stage(stage)
-
+print("Third Generation:")
+one_generation(stage)
+print("Fourth Generation:")
+print_stage(stage)
